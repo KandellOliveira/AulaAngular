d9831b8f91b65d3e82c207e93c8856998c002c6c
Kandell Oliveira <kandell.oliveira@esig.com.br>
2020-01-13 12:33:14 -0300

    Classe compilada alteração no componente

--- classe/classe.js
+++ classe/classe.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Animal = /** @class */ (function () {
     function Animal(nome) {
         this.nome = nome;
@@ -7,6 +20,17 @@ var Animal = /** @class */ (function () {
     };
     return Animal;
 }());
-var nomeAnimal = "Cachorro";
-var animal = new Animal(nomeAnimal);
-console.log(animal.recuperNome());
+var Cachorro = /** @class */ (function (_super) {
+    __extends(Cachorro, _super);
+    function Cachorro(nome) {
+        return _super.call(this, nome) || this;
+    }
+    return Cachorro;
+}(Animal));
+/* const cachorro = new Cachorro("Bob");
+const nomeCachorro = cachorro.recuperNome();
+console.log(nomeCachorro); */
+/* const nomeAnimal = "Cachorro";
+let animal = new Animal(nomeAnimal);
+
+console.log(animal.recuperNome()); */ 
